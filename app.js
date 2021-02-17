211b212c2b15316139ed3f15b929c68c2f287791
Louisterryn <terrynlouis@gmail.com>
2021-02-17 19:23:29 +0100

    finish

--- app.js
+++ app.js
@@ -2,18 +2,33 @@
 
 let shareBtn = document.querySelector('.div-share-btn');
 let sharingDiv = document.querySelector('.sharing');
+let sharingDivSmall = document.querySelector('.sharing-small');
 let svg = document.querySelector('.share-btn');
 
 shareBtn.addEventListener('click', function(){
-    if(sharingDiv.style.display === 'none'){
-        sharingDiv.style.display = 'flex';
-        shareBtn.style.background = 'hsl(217, 19%, 35%)';
-        svg.src = "images/icon-share white.svg";
-
-
+    // Condition taille écran
+    if(screen.width <= 1000){
+        // Condition display du sharing
+        if(sharingDivSmall.style.display === 'none'){
+            sharingDivSmall.style.display = 'flex';
+            shareBtn.style.background = 'hsl(214, 17%, 51%)';
+            svg.src = "images/icon-share white.svg";
+    
+    
+        }else{
+            sharingDivSmall.style.display = 'none';
+            shareBtn.style.background = 'hsl(210, 46%, 95%)';
+            svg.src = "images/icon-share.svg";
+        }
     }else{
-        sharingDiv.style.display = 'none';
-        shareBtn.style.background = 'hsl(210, 46%, 95%)';
-        svg.src = "images/icon-share.svg";
+        if(sharingDiv.style.display === 'none'){
+            sharingDiv.style.display = 'flex';
+            shareBtn.style.background = 'hsl(217, 19%, 35%)';
+            svg.src = "images/icon-share white.svg";
+        }else{
+            sharingDiv.style.display = 'none';
+            shareBtn.style.background = 'hsl(210, 46%, 95%)';
+            svg.src = "images/icon-share.svg";
+        }
     }
 });
